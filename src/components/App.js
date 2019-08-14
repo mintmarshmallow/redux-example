2045097c3e38cc3f482404966e4e5e1edcd6765a
mintmarshmallow <nopp054@naver.com>
2019-08-14 11:55:27 +0900

    asdf

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Counter from './Counter.js'
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+  render() {
+    return (
+      <Counter/>
+    )
+  }
+}
+
+
---
+++ src/components/Control.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+function createWarning(funcName) {
+    return () => console.warn(funcName+' is not defined')
+}
+
+export default class Control extends Component {
+  static propTypes = {
+    onPlus: PropTypes.func,
+    onSubtract: PropTypes.func,
+    onRandomizeColor: PropTypes.func
+  }
+  static defaultProps = {
+      onPlus: createWarning('onPlus'),
+      onSubTract: createWarning('onSubtract'),
+      onRandomizeColor: createWarning('onRAndomizeColor')
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.onPlus}>+</button>
+        <button onClick={this.props.onSubtract}>-</button>
+        <button onClick={this.props.onRandomizeColor}>Randomize Color</button>
+      </div>
+    )
+  }
+}
